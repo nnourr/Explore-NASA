222ada9de91b752b06b98f4dd80f3dd0672e9499
nnourr <nourqwerty123@gmail.com>
2022-01-17 13:33:14 -0500

    fix duplicate request bug

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import ImagesConainer from "./ImagesContainer"
 import styles from './appStyles.module.css'
 import React, { useState, useEffect } from "react"
-// import uuidv4 from 'uuid/v4'
-import { v4 as uuidv4 } from 'uuid';
 import {isMobile} from 'react-device-detect';
 
 function App() {
@@ -10,6 +8,7 @@ function App() {
 
 	function handleShowDescription(id) {
         const newImages = [...images]
+		console.log(newImages);
         const image = newImages.find(image => image.id === id)
         image.showDescription = !image.showDescription
         updateImages(newImages)
@@ -19,33 +18,44 @@ function App() {
         const newImages = [...images]
         const image = newImages.find(image => image.id === id)
         image.liked = !image.liked
+		let likedArray = JSON.parse(localStorage.getItem(LIKES_STORAGE_KEY))
+		if (likedArray === null) {
+			likedArray = []
+		}
+		if (image.liked) {
+			likedArray.push(image.data.url)
+		} else {
+			likedArray.splice(likedArray.indexOf(image.data.url))
+		}
+		localStorage.setItem(LIKES_STORAGE_KEY, JSON.stringify(likedArray))
         updateImages(newImages)
 	}
 	
 	const apiKey = process.env.REACT_APP_NASA_KEY;
 	
 	function generateImages(numOfImages) {
-		const offsetDate = new Date()
+		const offsetDate = images.length > 0 ? new Date(localStorage.getItem(DATE_STORAGE_KEY)) : new Date()
 		offsetDate.setDate(offsetDate.getDate()-images.length)
 		const startDate= new Date(offsetDate.toISOString())
 		startDate.setDate(startDate.getDate()-numOfImages + 1)
 
         fetchPhoto();
+		localStorage.setItem(DATE_STORAGE_KEY, startDate.toISOString())
 
 		async function fetchPhoto() {
 			const res = await fetch(
 				`https://api.nasa.gov/planetary/apod?start_date=${startDate.toISOString().split("T")[0]}&end_date=${offsetDate.toISOString().split("T")[0]}&api_key=${apiKey}`
 			);
 			const data = await res.json();
+			const likedPosts = JSON.parse(localStorage.getItem(LIKES_STORAGE_KEY))
 			for (let i = numOfImages-1; i >= 0; i--) {
+				let liked = false
+				if (likedPosts.includes(data[i].url)) liked = true
 				updateImages( prevImages=> {
-					return [...prevImages, {data:data[i], showDescription:false, id:uuidv4(), liked:false}]
+					return [...prevImages, {data:data[i], showDescription:false, id:data[i].url, liked:liked}]
 				})
 			}
-			
-			console.log(images);
 		}
-
 	}
 	
 	function loadOnScroll(e) {
@@ -60,9 +70,14 @@ function App() {
 			}, 10000);
 		}
 	}
-	
+
+	const LIKES_STORAGE_KEY = "nasaimage.likes"
+	const DATE_STORAGE_KEY = "nasaimage.date"
+
     useEffect(() => {
 		generateImages(10)
+		const currDate = new Date()
+		localStorage.setItem(DATE_STORAGE_KEY, currDate.toISOString())
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [])
 	return (
