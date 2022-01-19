6d8a706c831bb6d314a43cc780cd1672a4d0aaf5
nnourr <nourqwerty123@gmail.com>
2022-01-19 16:10:16 -0500

    mobile infinite scroll fix

--- src/ImagesContainer.js
+++ src/ImagesContainer.js
@@ -4,10 +4,13 @@ import styles from './ImagesContainerStyle.module.css'
 
 export default function ImagesContainer({images, handleShowDescription, handleLike}) {
     return (
-        <div className={styles.ImagesContainer}>
-            {images.map (image =>{
-                return <NasaImage image={image} key={image.id} handleShowDescription={handleShowDescription} handleLike={handleLike}></NasaImage>
-            })}
-        </div>
+        <>
+            <div className={styles.ImagesContainer}>
+                {images.map (image =>{
+                    return <NasaImage image={image} key={image.id} handleShowDescription={handleShowDescription} handleLike={handleLike}></NasaImage>
+                })}
+            </div>
+            <div className={styles.loading}></div>
+        </>
     )
 }
